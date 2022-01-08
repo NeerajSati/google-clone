eca657c69099b28c2b94cfaaf509a204af008ac2
Neeraj Sati <neeraj.sati123@gmail.com>
2022-01-08 20:02:41 +0530

    Videos Page Created

--- src/components/Newsresult/Newsresult.jsx
+++ src/components/Newsresult/Newsresult.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import './newsresult.css'
 function Newsresult() {
     return (
-        <div>
-            
+        <div className="allresults">
+        <div className="link">https://www.gulugulu.com</div>
+        <h2 className="linkTitle">Google Analytics</h2>
+        <div className='videoContent'>
+        <div className='videoLeft'>
+        <img className="videoThumbnail" src="https://www.google.com/chrome/static/images/download-browser/big_pixel_phone.png" alt="thumbnail"></img>
+        </div>
+        <div className="videoRight">
+        <p className="aboutVideo">Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.Google Analytics lets you measure your advertising ROI as well as track your Flash, video, and social networking sites and applications.</p>
+        </div>
+        </div>
         </div>
     )
 }
--- src/components/Videospage/Videospage.jsx
+++ src/components/Videospage/Videospage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './videospage.css'
 import Navbar from '../Navbar/Navbar';
 import Appbar from '../Appbar/Appbar';
+import Videosresult from '../Videosresult/Videosresult';
 
 function Videospage() {
     return (
@@ -10,6 +11,17 @@ function Videospage() {
         <Appbar/>
         <div className="mainContent">
         <div className="fetchTime">About 13,590,000,000 results in (0.56 seconds)</div>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
+        <Videosresult/>
         </div>
         </div>
     )
