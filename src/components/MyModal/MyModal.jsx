e5c78510b5e17039571388154f4698b281142fc2
Neeraj Sati <neeraj.sati123@gmail.com>
2022-01-10 13:23:00 +0530

    Voice Search Updated

--- src/components/MyModal/MyModal.jsx
+++ src/components/MyModal/MyModal.jsx
@@ -1,62 +1,40 @@
-import React,{useState,useEffect} from 'react'
-import './myModal.css'
-import {Modal,Button} from 'react-bootstrap';
-import GoogleDot from '../../img/googledots.gif'
-import GoogleDotStop from '../../img/googledotstop.png'
-import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-import { useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./myModal.css";
+import { Modal, Button } from "react-bootstrap";
+import GoogleDot from "../../img/googledots.gif";
+import GoogleDotStop from "../../img/googledotstop.png";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useNavigate } from "react-router-dom";
 
 function MyModal(props) {
-    const[search,setSearch] = useState('Click to Speak!');
-    const[searchText,setSearchText] = useState('');
-    console.log(!searchText);
-    const[stop,setStop] = useState(true);
-    const { transcript, resetTranscript } = useSpeechRecognition();
-    let navigate = useNavigate();
-    useEffect(()=>{
-        if(!stop){
-            SpeechRecognition.startListening();
-            setSearch(transcript);
-            setTimeout(() => {
-                setSearchText(transcript);
-            }, 5000);
-        }
-        else{
-            SpeechRecognition.stopListening();
-            setSearch('Click to Speak!');
-            resetTranscript();
-        }
-    },[stop,transcript]);
+  const [search, setSearch] = useState("");
+  const [stop, setStop] = useState(true);
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  let navigate = useNavigate();
 
-    useEffect(() => {
-      if(searchText){
-        navigate(`/${searchText}/all`);
+  useEffect(() => {
+    if (!stop) {
+      SpeechRecognition.startListening();
+      setSearch(transcript);
+      setTimeout(() => {
+        setStop(true);
+      }, 4000);
+    } else {
+      SpeechRecognition.abortListening();
+      setSearch("");
+      resetTranscript();
     }
-    }, [searchText]);
+  }, [stop, transcript]);
 
-    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-        return (
-            <Modal
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-          >
-          
-            <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                Voice Search...
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="justifyCenter">
-            Your Browser does not Support Speech Recognition.
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={props.onHide}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        );
-      }
+  useEffect(() => {
+    if (transcript) {
+      navigate(`/${transcript}/all`);
+    }
+  }, [stop]);
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return (
       <Modal
         {...props}
@@ -64,17 +42,13 @@ function MyModal(props) {
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
-      
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Voice Search...<p className="modalPara">{'('}Your Browser may not support Speech Recognition..{')'}</p>
+            Voice Search...
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className="justifyCenter">
-        {!stop && <h4>Listening...</h4>}
-          <h4>{search}</h4>
-          {stop && <img src={GoogleDotStop} onClick={()=>setStop(false)} className="googleDots" alt="listening"/>}
-          {!stop && <img src={GoogleDot} onClick={()=>setStop(true)} className="googleDots" alt="listening"/>}
+          Your Browser does not Support Speech Recognition.
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
@@ -82,5 +56,47 @@ function MyModal(props) {
       </Modal>
     );
   }
-  
-export default MyModal
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Voice Search...
+          <p className="modalPara">
+            {"("}Your Browser may not support Speech Recognition..{")"}
+          </p>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="justifyCenter">
+        {!stop && <h4>Listening...</h4>}
+        {stop && <h4>Click to Speak!</h4>}
+        {!stop && <h4>{search}</h4>}
+        {stop && (
+          <img
+            src={GoogleDotStop}
+            onClick={() => setStop(false)}
+            className="googleDots"
+            alt="listening"
+          />
+        )}
+        {!stop && (
+          <img
+            src={GoogleDot}
+            onClick={() => setStop(true)}
+            className="googleDots"
+            alt="listening"
+          />
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default MyModal;
